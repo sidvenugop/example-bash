f27e11e31f61c2729f6949d87e9636e543312800
sidvenugop <38498021+sidvenugop@users.noreply.github.com>
2018-05-24 23:19:22 +0530

    Update application.sh

--- application.sh
+++ application.sh
@@ -5,15 +5,5 @@
 # * Linux Host
 # * MAC OS X Host
 #
-nodename=`uname -n`
-arch=`uname -m`
-datetime=`date +"%u %m/%d/%Y %H:%M:%S.%N"`
-echo "Hello World from Distelli."
-echo "You have successfully deployed a simple application script."
-echo "Server: $nodename"
-echo "Server Architecture: $arch"
-echo "Server Time: $datetime"
-echo "Manifest Environment Variable: $EXAMPLE"
-echo ""
-sleep 10
+echo "Cola" > /etc/lotto
 
