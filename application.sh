241726b880b4295172e8473f3b82d2918289faa6
sidvenugop <38498021+sidvenugop@users.noreply.github.com>
2018-05-28 22:26:47 +0530

    Update application.sh

--- application.sh
+++ application.sh
@@ -14,4 +14,6 @@ fi
 sudo rpm -ivh puppet-agent-5.3.5-1.el7.x86_64.rpm
 sudo systemctl start puppet 
 systemctl status puppet
-
+cd /etc/puppetlabs/code/environments/production/modules/
+git clone https://github.com/sidvenugop/helloworld
+puppet apply --modulepath=/etc/puppetlabs/code/environments/production/modules -e "include helloworld" --noop
