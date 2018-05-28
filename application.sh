721b03ef7f3b4eaf4ad4797218d150f38e5d2005
sidvenugop <38498021+sidvenugop@users.noreply.github.com>
2018-05-28 22:12:55 +0530

    Update application.sh

--- application.sh
+++ application.sh
@@ -5,10 +5,11 @@
 # * Linux Host
 # * MAC OS X Host
 #
-cd ~
 if [ -d "/etc/puppetlabs" ]; then
 echo "Puppet Already Installed"
 else
 yum install wget
 wget https://s3.amazonaws.com/puppet-agents/2017.3/puppet-agent/5.3.5/repos/el/7/PC1/x86_64/puppet-agent-5.3.5-1.el7.x86_64.rpm
 fi
+rpm -Uvh puppet-agent-5.3.5-1.el7.x86_64.rpm
+service puppet status
