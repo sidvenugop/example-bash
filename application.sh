db623d4212deb37ca10b2255dd01d1a4d252dc1c
sidvenugop <38498021+sidvenugop@users.noreply.github.com>
2018-05-28 23:37:38 +0530

    Update application.sh

--- application.sh
+++ application.sh
@@ -10,11 +10,11 @@ echo "Puppet Already Installed"
 else
 sudo yum install wget -y
 sudo wget https://s3.amazonaws.com/puppet-agents/2017.3/puppet-agent/5.3.5/repos/el/7/PC1/x86_64/puppet-agent-5.3.5-1.el7.x86_64.rpm
-fi
 sudo rpm -ivh puppet-agent-5.3.5-1.el7.x86_64.rpm
-bash
 sudo systemctl start puppet 
 systemctl status puppet
+fi
+bash
 if [ -d "/usr/bin/git" ]; then
 echo "Git Already Installed"
 else
