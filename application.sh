8ba5b276bb9bc440379aa97cda4cd291dde22854
sidvenugop <38498021+sidvenugop@users.noreply.github.com>
2018-05-28 22:24:10 +0530

    Update application.sh

--- application.sh
+++ application.sh
@@ -12,6 +12,6 @@ yum install wget
 wget https://s3.amazonaws.com/puppet-agents/2017.3/puppet-agent/5.3.5/repos/el/7/PC1/x86_64/puppet-agent-5.3.5-1.el7.x86_64.rpm
 fi
 sudo rpm -ivh puppet-agent-5.3.5-1.el7.x86_64.rpm
-service puppet start
+sudo systemctl start puppet 
 systemctl status puppet
 
