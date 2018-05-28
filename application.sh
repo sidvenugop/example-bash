931c5af21ad8ab372069d52c510f581fa2ff4c87
sidvenugop <38498021+sidvenugop@users.noreply.github.com>
2018-05-28 22:42:14 +0530

    Update application.sh

--- application.sh
+++ application.sh
@@ -8,12 +8,17 @@
 if [ -d "/etc/puppetlabs" ]; then
 echo "Puppet Already Installed"
 else
-yum install wget
-wget https://s3.amazonaws.com/puppet-agents/2017.3/puppet-agent/5.3.5/repos/el/7/PC1/x86_64/puppet-agent-5.3.5-1.el7.x86_64.rpm
+sudo yum install wget -y
+sudo wget https://s3.amazonaws.com/puppet-agents/2017.3/puppet-agent/5.3.5/repos/el/7/PC1/x86_64/puppet-agent-5.3.5-1.el7.x86_64.rpm
 fi
 sudo rpm -ivh puppet-agent-5.3.5-1.el7.x86_64.rpm
+bash
 sudo systemctl start puppet 
 systemctl status puppet
+if [ -d "/usr/bin/git" ]; then
+echo "Git Already Installed"
+else
+sudo yum install git -y
 cd /etc/puppetlabs/code/environments/production/modules/
-git clone https://github.com/sidvenugop/helloworld
+sudo git clone https://github.com/sidvenugop/helloworld
 puppet apply --modulepath=/etc/puppetlabs/code/environments/production/modules -e "include helloworld" --noop
